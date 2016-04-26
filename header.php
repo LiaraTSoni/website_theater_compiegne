f1bb1f90dda3709c0bfd01514a9c1faf5dacb319
Valentin Coppin <valimero.ilou@gmail.com>
2016-04-26 11:58:13 +0200

    include header in index

---
+++ header.php
@@ -0,0 +1,24 @@
+		<div id="header" class="postfix">
+			<img id="logo" src="./src/logo.jpg" alt="Logo" class="l_float">
+			<!-- Logo -->
+			<div id="navigation" class="l_float">
+				<ul>
+					<li>
+						<a href="">Accueil</a>
+					</li>
+					<li>
+						<a href="">La saison</a>
+					</li>
+					<li>
+						<a href="">Billeterie</a>
+					</li>
+					<li>
+						<a href="">Infos pratiques</a>
+					</li>
+					<li>
+						<a href="">Contact</a>
+					</li>
+				</ul>
+			</div>
+		</div>
+			<div class="clearfix"></div>
