17d511de2ff038b1cb70aafae07d2ef6bf953328
Valentin Coppin <valimero.ilou@gmail.com>
2016-04-26 12:04:14 +0200

    create aside.php and header.php

---
+++ aside.php
@@ -0,0 +1,10 @@
+<div id="aside">
+	<h2>Prochain Spectacle</h2>
+	<p>
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum sint velit vero, temporibus, ut libero autem expedita eum perferendis, quisquam odio, ex. Vel odit obcaecati ipsum, fuga nemo perspiciatis sunt.
+	</p>
+	<p class="date_aside">Le Jeudi 5 Février</p>
+	<a href="#">
+		<img src="./src/aside.jpg" alt="Affiche pour faire feu de tout bois"></a>
+
+</div>
---
+++ footer.php
@@ -0,0 +1,33 @@
+	<div id="footer">
+		<div id="footer_informations" class="l_float">
+			<span>Adresse</span>
+			<br />
+			<span>Tel</span>
+			<br />
+			<span>Contact</span>
+		</div>
+		<div class="v_line l_float"></div>
+		<div id="footer_bas" class="l_float">
+			<ul>
+				<li>
+					<a href="">CGV</a>
+				</li>
+				<li>
+					<a href="">Plan du site</a>
+				</li>
+				<li>
+					<a href="">mentions légales</a>
+				</li>
+				<li>
+					<a href="">crédits</a>
+				</li>
+			</ul>
+		</div>
+		<div class="v_line l_float"></div>
+		<div id="reseaux_sociaux" class="l_float">
+			<div class="facebook"></div>
+			<div class="twitter"></div>
+			<div class="google"></div>
+		</div>
+		<div class="clearfix"></div>
+	</div>
