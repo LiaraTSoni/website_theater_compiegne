7df62abd1131b9652d6cc5f41f054b17c9a5e00c
Valentin Coppin <valimero.ilou@gmail.com>
2016-04-26 20:16:11 +0200

    add others pages

---
+++ billeterie.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+	<meta charset="UTF-8">
+	<title>Billeterie - Le Nouveau Théâtre de Compiègne</title>
+	<link rel="stylesheet" href="css/styles.css">
+</head>
+<body>
+	<div id="container">
+		<?php include("include/header.php"); ?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+		<?php include("include/footer.php"); ?>
+	</div>
+</body>
+</html>
