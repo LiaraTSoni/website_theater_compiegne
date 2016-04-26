923b4f2572b4bc6634754906101ebd0e16bf5079
Valentin Coppin <valimero.ilou@gmail.com>
2016-04-26 20:01:26 +0200

    indent code

--- index.php
+++ index.php
@@ -3,11 +3,11 @@
 <head>
 	<meta charset="UTF-8">
 	<!-- jQuery library (served from Google) -->
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-<!-- bxSlider Javascript file -->
-<script src="./lib/bxslider/jquery.bxslider.min.js"></script>
-<!-- bxSlider CSS file -->
-<link href="./lib/bxslider/jquery.bxslider.css" rel="stylesheet" />
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+	<!-- bxSlider Javascript file -->
+	<script src="./lib/bxslider/jquery.bxslider.min.js"></script>
+	<!-- bxSlider CSS file -->
+	<link href="./lib/bxslider/jquery.bxslider.css" rel="stylesheet" />
 	<title>Le Nouveau Théâtre de Compiègne</title>
 	<link href='https://fonts.googleapis.com/css?family=Oswald:700,400,300&amp;subset=latin,latin-ext' rel='stylesheet' type='text/css'>
 	<link rel="stylesheet" href="./css/styles.css"></head>
@@ -16,17 +16,26 @@
 		<!-- __________________Header______________________ -->
 		<?php include("include/header.php"); ?>
 
-
 		<!-- _____________________________________________________ -->
 
 		<div id="slider" >
-		<ul class="bxslider">
-			<li><img src="./src/slider/1.jpg" alt="slide 1" /></li>
-			<li><img src="./src/slider/2.jpg" alt="slide 2" /></li>
-			<li><img src="./src/slider/3.jpg" alt="slide 3" /></li>
-			<li><img src="./src/slider/4.jpg" alt="slide 4" /></li>
-			<li><img src="./src/slider/5.jpg" alt="slide 5" /></li>
-		</ul>
+			<ul class="bxslider">
+				<li>
+					<img src="./src/slider/1.jpg" alt="slide 1" />
+				</li>
+				<li>
+					<img src="./src/slider/2.jpg" alt="slide 2" />
+				</li>
+				<li>
+					<img src="./src/slider/3.jpg" alt="slide 3" />
+				</li>
+				<li>
+					<img src="./src/slider/4.jpg" alt="slide 4" />
+				</li>
+				<li>
+					<img src="./src/slider/5.jpg" alt="slide 5" />
+				</li>
+			</ul>
 		</div>
 
 		<!-- _____________________________________________________ -->
@@ -51,10 +60,10 @@ Au théâtre, il ne faut pas se fier au vocabulaire, ainsi la servante n’est p
 				<div class="texte_article">
 					<h1>Cirqu'en famille</h1>
 					<p class="lettrine">
-
 						Ateliers ludiques pour vivre en famille un moment de découverte et d'expression artistique autour du langage et du geste circassien. Jeux d'équilibre, jeux dans l'espace et manipulation d'objets que vous pourrez refaire à la maison !
-6-8 ans <br />
-Ateliers menés par Michèle d'Angelo, artiste circassienne, et Abel Chahbi, jongleur et manipulateur d'objets ou par Fiona Couster, intervenante cirque
+6-8 ans
+						<br />
+						Ateliers menés par Michèle d'Angelo, artiste circassienne, et Abel Chahbi, jongleur et manipulateur d'objets ou par Fiona Couster, intervenante cirque
 					</p>
 					<p class="date_article">Mardi 5 mars / 18h</p>
 				</div>
@@ -69,13 +78,11 @@ Ateliers menés par Michèle d'Angelo, artiste circassienne, et Abel Chahbi, jon
 			<div class="clearfix"></div>
 		</div>
 
-<?php include("include/footer.php"); ?>
+		<?php include("include/footer.php"); ?></div>
+	<!-- Aside -->
+	<?php include("include/aside.php"); ?>
 
-</div>
-<!-- Aside -->
-<?php include("include/aside.php"); ?>
-
-<script>
+	<script>
 $(document).ready(function(){
   $('.bxslider').bxSlider();
 });
