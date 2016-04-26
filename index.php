479875825b6c5f9718d31a387cb173881e377d91
Valentin Coppin <valimero.ilou@gmail.com>
2016-04-26 12:21:37 +0200

    modif header

--- index.php
+++ index.php
@@ -54,60 +54,13 @@ Ateliers menés par Michèle d'Angelo, artiste circassienne, et Abel Chahbi, jon
 			</div>
 			<div class="clearfix"></div>
 		</div>
-		<!--_____________________________________________________ -->
 
-		<!--  		<div id="youtube" align="center">
-		<iframe width="550" height="400" src="https://www.youtube.com/embed/C0GMVyng0Dc" frameborder="0" allowfullscreen></iframe>
 
-	</div>
-	-->
-	<!-- _____________________________________________________ -->
-
-	<div id="footer">
-		<div id="footer_informations" class="l_float">
-			<span>Adresse</span>
-			<br />
-			<span>Tel</span>
-			<br />
-			<span>Contact</span>
-		</div>
-		<div class="v_line l_float"></div>
-		<div id="footer_bas" class="l_float">
-			<ul>
-				<li>
-					<a href="">CGV</a>
-				</li>
-				<li>
-					<a href="">Plan du site</a>
-				</li>
-				<li>
-					<a href="">mentions légales</a>
-				</li>
-				<li>
-					<a href="">crédits</a>
-				</li>
-			</ul>
-		</div>
-		<div class="v_line l_float"></div>
-		<div id="reseaux_sociaux" class="l_float">
-			<div class="facebook"></div>
-			<div class="twitter"></div>
-			<div class="google"></div>
-		</div>
-		<div class="clearfix"></div>
-	</div>
+<?php include("footer.php"); ?>
 
 </div>
 <!-- Aside -->
-<div id="aside">
-	<h2>Prochain Spectacle</h2>
-	<p>
-		Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum sint velit vero, temporibus, ut libero autem expedita eum perferendis, quisquam odio, ex. Vel odit obcaecati ipsum, fuga nemo perspiciatis sunt.
-	</p>
-	<p class="date_aside">Le Jeudi 5 Février</p>
-	<a href="#">
-		<img src="./src/aside.jpg" alt="Affiche pour faire feu de tout bois"></a>
+<?php include("aside.php"); ?>
 
-</div>
 </body>
 </html>
