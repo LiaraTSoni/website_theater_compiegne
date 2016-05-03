ce6d308ab3dd6bff30096b87c2af6abec69f495a
Valentin Coppin <valimero.ilou@gmail.com>
2016-05-03 12:26:54 +0200

    add infos_pratiques: \n-google_map\ntext

--- infos_pratiques.php
+++ infos_pratiques.php
@@ -10,7 +10,56 @@
 		<?php include("include/header.php"); ?>
 
 
+	<div id="content">
+		<div id="google_map">
+			<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2595.4298634806846!2d2.828224615466081!3d49.419686668974826!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e7d67d9b62e855%3A0xc3712d11878b7088!2zVGjDqcOidHJlIGltcMOpcmlhbCBkZSBDb21wacOoZ25l!5e0!3m2!1sfr!2sfr!4v1462270390856" width="800" height="600" frameborder="0" style="border:0" allowfullscreen></iframe>
+		</div>
+		<div id="article_infos">
+			<p>Théâtre des Champs-Elysées
+15 avenue Montaigne
+<br />
+75008 PARIS
+<br />
+<br />
+Téléphone réservation 01 49 52 50 50
+<br />
+Téléphone administration 01 49 52 50 00
+<br />
+Objets trouvés 01 49 52 50 59
+<br />
 
+<br />
+Métro station Alma-Marceau (ligne 9) ou Franklin-Roosevelt (ligne 1)
+<br />
+RER station Pont de l'Alma (ligne C)
+<br />
+Bus lignes 42, 63, 72, 80, 92
+<br />
+
+<br />
+Station de taxi Place de l'Alma, angle avenue George V
+<br />
+Station Vélib' en face du Théâtre
+<br />
+
+<br />
+Parc de stationnement Alma-George V 
+<br />
+
+<br />
+Public à mobilité réduite
+<br />
+Comment accéder au Théâtre : 
+<br />
+De la rue à l’entrée : Le Théâtre des Champs-Elysées est équipé d’une rampe d’accès pour les fauteuils roulants. Elle est située sur la partie gauche de la façade.
+<br />
+De l’entrée au hall : une plateforme motorisée amène au hall.
+<br />
+Du hall jusque dans la salle : des contrôleurs accompagnent votre placement en salle.</p>
+<br />
+		</div>
+	</div>
+	
 
 
 
